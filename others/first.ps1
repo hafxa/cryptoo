c77e0427bafa3b0e1bc115162667a53c82475f1d
Hafsa <hafsairfan964@gmail.com>
2026-07-23 22:11:34 +0100

    Added username field

--- others/first.ps1
+++ others/first.ps1
@@ -4,6 +4,8 @@ foreach($line in Get-Content .\first.txt){
 
     $firstname = $line.split(' ')[0]
     $lastname = $line.split(' ')[1]
+    #We are also creating a username for simplicity
+    $usr = ($line[0] + $lastname).tolower()
 
     Write-Host $lastname
     #Just randomly generating a password
@@ -13,6 +15,7 @@ foreach($line in Get-Content .\first.txt){
     #Verifying that the user does not already exist
     if (!(Get-AdUser -Filter "sAMAccountName -eq '$($line)'")) {
         New-AdUser -Name $line
+                   -UserPrincipalName $usr
                    -GivenName $firstname
                    -Surname $lastname
                    -AccountPassword (ConvertTo-SecureString -AsPlainText $pass -Force)
@@ -20,7 +23,5 @@ foreach($line in Get-Content .\first.txt){
                    -Enabled $true
         
     }
-
-
 }
 
