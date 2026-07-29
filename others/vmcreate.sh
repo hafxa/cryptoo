a7186373c5dadf3f957d57e3b556ac1671f255f9
Hafsa <hafsairfan964@gmail.com>
2026-07-29 18:26:57 +0100

    Added network adapter modification options

--- others/vmcreate.sh
+++ others/vmcreate.sh
@@ -8,16 +8,24 @@
 
 echo 'Welcome to the VM creation script. We just need a few details from you..'
 read -p 'Name: ' name
-
 read -p 'To enter the OStype, press 1, to list OStypes, press 2: ' selection
+
 #Now, what else do we need?
 if [ "$selection" = "1" ]; then
     read -p 'OSType: ' ostype
 elif [[ "$selection" == "2" ]]; then
     VBoxManage list ostypes
+    #Need to add functionality here for if the user wants to now select the ostype??
 else 
     echo "Unkown value entered, please re-run script and try again."
     exit 2
 fi
-# VBoxMange createvm --name $name `
-#                    -- 
+
+read -p 'if you wish to add/modify network adatpters, enter 1' net
+
+if [ "$net"=="1" ]; then
+    read -a 'Enter the adapter and type you wish to modify: ' type
+    VBoxManage modifyvm --name --nic${type[0]} ${type[1]} --nic\{type[0]\}type
+
+VBoxMange createvm --name $name ` --register --basefolder '/home/hafsa01/VirtualBox\ VMs'
+read -p 'VM created! If you wish to start the VM now, enter 1'
